7e25fce4cac562f3bdcb5b09fcdfec18a5974c94
Laura Pino <lpigueroa@gmail.com>
2024-02-19 17:36:59 +0100

    routes

--- src/views/manga/OneMangaView.vue
+++ src/views/manga/OneMangaView.vue
@@ -6,6 +6,29 @@
       <div class="container__left__spam">Spam!</div>
       <div class="container__left__spam">Spam!</div>
     </div>
+    <div class="container__manga">
+      <div class="container__manga__head">
+        <AdvancedImage :cld-img="jujutsuFront" />
+        <p>
+          La historia gira en torno al estudiante Yūji Itadori, quien se une a
+          una organización secreta de hechiceros para matar a una poderosa
+          maldición llamada Ryomen Sukuna tras convertirse en su anfitrión.
+        </p>
+      </div>
+      <div class="container__manga__episodes">
+        <ul>
+          <li class="container__manga__episodes__item">Episodio 1</li>
+
+          <li class="container__manga__episodes__item">Episodio 2</li>
+
+          <li class="container__manga__episodes__item">Episodio 3</li>
+
+          <li class="container__manga__episodes__item">Episodio 4</li>
+
+          <li class="container__manga__episodes__item">Episodio 5</li>
+        </ul>
+      </div>
+    </div>
     <div class="container__right">
       <div class="container__right__spam">Spam!</div>
       <div class="container__right__spam">Spam!</div>
@@ -14,14 +37,27 @@
   </div>
 </template>
 <script>
+import { cld } from "@/services/cloudinary/cloudinaryConfig";
+import { AdvancedImage } from "@cloudinary/vue";
+import { thumbnail } from "@cloudinary/url-gen/actions/resize";
+
+const jujutsuFront = cld.image("portadas/JujutsuFront");
+
+jujutsuFront.resize(thumbnail().width(140).height(210));
+
 export default {
   name: "OneManga",
+  components: {
+    AdvancedImage,
+  },
 
   data() {
     return {
+      jujutsuFront,
       routeId: null,
     };
   },
+
   mounted() {
     this.routeId = this.$route.params.name;
   },
